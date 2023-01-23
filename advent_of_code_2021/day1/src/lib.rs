4d8fd3360718af98561dd2d623689a3759bd0cb5
Vince P <32515747+Peperito@users.noreply.github.com>
2023-01-23 19:27:23 +0100

    day1

--- advent_of_code_2021/day1/src/lib.rs
+++ advent_of_code_2021/day1/src/lib.rs
@@ -1,15 +1,43 @@
 pub fn part_a(input: &str) -> i64 {
-    let mut a = 0;
-    for line in input.trim().split('\n') {
-        a += 1
+    let mut depth_increases = 0;
+    let measures_vector: Vec<u32> = input.trim()
+            .split('\n')
+            .map(|c| c.parse::<u32>().unwrap())
+            .collect();
+    for i in 1..measures_vector.len() {
+        if measures_vector[i] > measures_vector[i - 1] {
+            depth_increases += 1
+        }
     }
-    a
+    depth_increases
 }
+
+pub fn part_b(input: &str) -> i64 {
+    let mut depth_increases = 0;
+    let measures_vector: Vec<u32> = input.trim()
+            .split('\n')
+            .map(|c| c.parse::<u32>().unwrap())
+            .collect();
+
+    for i in 3..measures_vector.len() {
+        if &measures_vector[i-2..=i].iter().sum::<u32>() 
+        > &measures_vector[i-3..=i-1].iter().sum::<u32>() {
+            depth_increases += 1;
+        }
+    }
+    depth_increases
+}
+
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn part_a() {
-        assert_eq!(super::part_a(include_str!("input.txt")), 3);
+        assert_eq!(super::part_a(include_str!("input.txt")), 1226);
+    }
+
+    #[test]
+    fn part_b() {
+        assert_eq!(super::part_b(include_str!("input.txt")), 8);
     }
 }
