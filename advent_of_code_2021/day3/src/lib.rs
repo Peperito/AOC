63ef8060cc1cc6a15a32c6aead8eb25336f3f207
Vince P <32515747+Peperito@users.noreply.github.com>
2023-01-30 18:27:07 +0100

    day_3_start

---
+++ advent_of_code_2021/day3/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn part_a(input: &str) -> u32 {
+    for line in input.trim().split('\n') {
+
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_a() {
+        assert_eq!(super::part_a(include_str!("testinput.txt")), 0);
+    }
+}
