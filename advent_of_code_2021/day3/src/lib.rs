58ee10bf2709229f3f63a41a67e00c2e3cf8c808
Vince P <32515747+Peperito@users.noreply.github.com>
2023-02-01 20:44:56 +0100

    day_3_part_b

--- advent_of_code_2021/day3/src/lib.rs
+++ advent_of_code_2021/day3/src/lib.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
+
 static BASE: u32 = 2;
 
-//Works but did not realise from test input that there 
-//would be 12 bits per line (test has only 4)
+//Works but ugly
 pub fn part_a(input: &str) -> u32 {
 
     let mut total_lines = 0;
@@ -32,7 +33,6 @@ pub fn part_a(input: &str) -> u32 {
 
     let mut gamma_rate: u32 = 0;
     for i in 0..=11 {
-        println!("{}", bits_vec[11 - i] as u32);
         gamma_rate += bits_vec[i] as u32 * BASE.pow((11 - i) as u32);
     }
 
@@ -42,42 +42,31 @@ pub fn part_a(input: &str) -> u32 {
 }
 
 pub fn part_b(input: &str) -> u32 {
+    
+    let lines: Vec<String> = input.trim().split('\n').collect();
+    let mut ones: Vec<u32> = Vec::new(); 
+    let mut zeros: Vec<u32> = Vec::new();
+    let threshold = i / 2;
 
-    let mut total_lines = 0;
-    let mut bits_pop: [u32; 12] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    for line in input.trim().split('\n') {
+    for (i, line) in lines.enumerate() {
         let mut number: u32 = 0;
-        for i in 0..line.len() {
+        for k in 0..line.len() {
             number += line.chars()
                 .nth(i)
                 .unwrap()
                 .to_digit(10)
                 .unwrap() * BASE.pow(i as u32);
-        }
-        
-        for i in 0..line.len() {
-            if number & (1 << i ) != 0 {
-                bits_pop[i] += 1;
+            if number & (1 << k ) != 0 {
+                ones.push(number);
+            } else {
+                zeros.push(number);
             }
         }
-        total_lines += 1;
+
+        for k in 0..line.len() {
+        }
     }
-
-    let bits_vec: Vec<bool> = bits_pop.iter().map(|value| {
-        if value * 2 >= total_lines {true} else {false}
-        })
-        .collect();
-
-    let mut gamma_rate: u32 = 0;
-    for i in 0..=11 {
-        println!("{}", bits_vec[11 - i] as u32);
-        gamma_rate += bits_vec[i] as u32 * BASE.pow((11 - i) as u32);
-    }
-
-    let mask = (1 << 12) - 1;
-
-    gamma_rate as u32 * (!gamma_rate as u32 & mask)
+    0
 }
 
 #[cfg(test)]
@@ -85,6 +74,6 @@ mod tests {
 
     #[test]
     fn test_a() {
-        assert_eq!(super::part_a(include_str!("input.txt")), 2035764);
+        assert_eq!(super::part_b(include_str!("testinput.txt")), 230);
     }
 }
